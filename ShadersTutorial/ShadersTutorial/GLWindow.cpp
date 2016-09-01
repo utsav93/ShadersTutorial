3396a9653a90740d94129e44cd38412156a65155
Utsav Jamwal <nick.utsav2345@gmail.com>
2016-08-31 21:27:49 -0600

    Two Triangles, fragment shader being used to color.

--- ShadersTutorial/ShadersTutorial/GLWindow.cpp
+++ ShadersTutorial/ShadersTutorial/GLWindow.cpp
@@ -1,11 +1,12 @@
 #include <gl\glew.h>
 #include <GLWindow.h>
 
+extern const char* vertexShaderCode;
+extern const char* fragmentShaderCode;
 
-void GLWindow::initializeGL()
+void sendDataToOpenGL()
+
 {
-	glewInit();
-	
 	//vertex data
 	GLfloat verts[] =
 	{
@@ -17,14 +18,14 @@ void GLWindow::initializeGL()
 		+1.0f, +0.0f, +0.0f,
 		-1.0f, -1.0f,
 		+1.0f, +0.0f, +0.0f,
-		+1.0f, -1.0f
+		+1.0f, -1.0f,
 		+1.0f, +0.0f, +0.0f,
 	};
 
 	//array buffer setup
-	GLuint  vertexmyBufferID;
-	glGenBuffers(1, &vertexmyBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexmyBufferID);
+	GLuint  vertexMyBufferID;
+	glGenBuffers(1, &vertexMyBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexMyBufferID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts),
 		verts, GL_STATIC_DRAW);
 
@@ -34,7 +35,7 @@ void GLWindow::initializeGL()
 
 	//color
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) *2 ));
 
 	//element array buffer setup
 	GLushort indices[] = { 0,1,2, 0,3,4 };
@@ -43,6 +44,118 @@ void GLWindow::initializeGL()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
 		indices, GL_STATIC_DRAW);
+}
+
+void checkGlProgram(GLuint prog, const char *file, int line)
+{
+	GLint status;
+	glGetProgramiv(prog, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE) {
+		int loglen;
+		char logbuffer[1000];
+		glGetProgramInfoLog(prog, sizeof(logbuffer), &loglen, logbuffer);
+		fprintf(stderr, "OpenGL Program Linker Error at %s:%d:\n%.*s", file, line, loglen, logbuffer);
+	}
+	else {
+		int loglen;
+		char logbuffer[1000];
+		glGetProgramInfoLog(prog, sizeof(logbuffer), &loglen, logbuffer);
+		if (loglen > 0) {
+			fprintf(stderr, "OpenGL Program Link OK at %s:%d:\n%.*s", file, line, loglen, logbuffer);
+		}
+		glValidateProgram(prog);
+		glGetProgramInfoLog(prog, sizeof(logbuffer), &loglen, logbuffer);
+		if (loglen > 0) {
+			fprintf(stderr, "OpenGL Program Validation results at %s:%d:\n%.*s", file, line, loglen, logbuffer);
+		}
+	}
+}
+
+void installShaders()
+{
+	GLenum err = GL_NO_ERROR;
+	//Vertex Shader Object
+	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+
+	//Fragment Shader Object
+	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//array of pointers
+	const char* adapter[1];
+	//points to character string(vertex) defined in shadercode.cpp
+	adapter[0] = vertexShaderCode;
+	glShaderSource(vertexShaderID, 1, adapter, 0);
+
+	adapter[0] = fragmentShaderCode;
+	glShaderSource(fragmentShaderID, 1, adapter, 0);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+	glCompileShader(vertexShaderID);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+	glCompileShader(fragmentShaderID);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+
+	GLchar dataBuffer[256];
+	GLsizei returnSize;
+	GLint dumbReturn;
+
+	glGetShaderInfoLog(vertexShaderID, 256, &returnSize, dataBuffer);
+	glGetShaderiv(vertexShaderID, GL_COMPILE_STATUS, &dumbReturn);
+
+	if (dumbReturn == GL_TRUE)
+	{
+		printf("Ok");
+	}
+
+	else if (dumbReturn == GL_FALSE)
+	{
+		printf("Why?S");
+	}
+	else
+	{
+		printf("kill me now");
+	}
+	
+	GLuint programID = glCreateProgram();
+	glAttachShader(programID, vertexShaderID);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+	glAttachShader(programID, fragmentShaderID);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+	glLinkProgram(programID);
+	checkGlProgram(programID, __FILE__, __LINE__);
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+	glUseProgram(programID);
+	
+	if ((err = glGetError()) != GL_NO_ERROR)
+	{
+		printf("%x%d", err, __LINE__);
+	}
+}
+
+
+void GLWindow::initializeGL()
+{
+	glewInit();
+	sendDataToOpenGL();
+	installShaders();
+	
 
 }
 
