09c140490d4f7ec65b49485dfc69ca1fe5e48c1e
Utsav Jamwal <nick.utsav2345>
2016-12-13 20:38:59 -0700

    Obj exporter in a renderer. Courtesy: Vega bai!

--- ShadersTutorial/ShadersTutorial/FragmentShadowShader.glsl
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-out float fragmentDepth;
-
-void main()
-{
-	fragmentDepth = gl_FragCoord.z;
-}
--- ShadersTutorial/ShadersTutorial/Vertex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <glm.hpp>
-struct Vertex
-{
-	glm::vec3 position;
-	glm::vec3 color;
-	glm::vec3 normal;
-	glm::vec2 UV;
-};
