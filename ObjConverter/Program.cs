60b5df9f9c3b835e3cc25130def6a841cc75da3e
Utsav Jamwal <nick.utsav2345@gmail.com>
2016-12-14 22:32:47 -0700

    added files to ignore list

--- ObjConverter/Program.cs
+++ ObjConverter/Program.cs
@@ -30,8 +30,8 @@ class MyClass
         //    return 0;
         try
         {
-            args[0] =  "Ogre.obj";
-            args[1] = "Ogre.bin";
+            //args[0] = "Ogre.obj";
+            //args[1] = "Ogre.bin";
             Console.WriteLine("Reading obj file...");
             ReadObjData(args[0]);
             // RotateScene(); // For Maya files
